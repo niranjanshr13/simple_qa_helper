68a7d0005b52efd85283f747ba7965e13653a498
Niranjan Shrestha <z@192.168.0.53>
2023-11-04 21:07:32 -0400

    fixing

---
+++ Readme.md
@@ -0,0 +1,10 @@
+# Example:
+python3 qa.py -f yaml-file.yml
+
+# Tips
+question should be in double quotes
+
+no '---' at the end
+
+options are only numeric, can handle i.e, "1" or "1,2".
+
--- qa.py
+++ qa.py
@@ -1,27 +1,35 @@
 #!/bin/python3
-import yaml, os
+import yaml, os, argparse
+parser = argparse.ArgumentParser(description='Quiz App')
+parser.add_argument('-f', '--file', help='YAML file to load', required=True)
+args = parser.parse_args()
+
 os.system('clear')
-lsts = yaml.load_all(open('yaml.yml','r'), Loader=yaml.FullLoader)
+lsts = yaml.load_all(open(args.file,'r'), Loader=yaml.FullLoader)
 
 coll = [ x for x in lsts ]
 correct = 0
-for qnum, lst in enumerate(coll):
+for qnum, lst in tqdm(enumerate(coll):
     question = lst['q']
     options = lst['o']
+    answers = str(lst['a'])
     print(f"[{qnum}/{len(coll)}]")
     print(question)
-    coll_options = []
 
+    coll_options = []
     for num, option in enumerate(options):
-        
         print(f"{num}: {option}")
         coll_options.append(str(num))
     choose = input(f"Enter {','.join(coll_options)}: ")
-    if int(choose) == lst['a']:
+    if ',' in answers:
+        print(f"There are {list(answers).count(',') + 1} options")
+    if choose == answers:
         print("Correct")
         correct += 1
     else:
-        print('nop')
+        print('Incorrect')
     input("Press Enter to continue...")
     os.system('clear')
+    continue
+os.system('clear')
 print(f"Correct: {correct}/{len(coll)}")
