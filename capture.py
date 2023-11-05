cab966945276b0c5f0868d0ecbf527ae580f1b31
Niranjan Shrestha <z@192.168.0.53>
2023-11-04 21:08:57 -0400

    fixing

--- capture.py
+++
@@ -1,9 +0,0 @@
-# setup using a i3wm keybind.
-# trigger a new qa, starting with question
-# send a question to buffer
-# trigger options from keybind
-# send a option to buffer
-# send a correct answer by number
-# show a buffer
-# verify if everything is ok
-# send to qa bank.
